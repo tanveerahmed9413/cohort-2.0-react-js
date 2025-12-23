ff65ff3e0e0270492ddcf76e73e77f9b342f34c0
tanveerahmed9413 <tanveerahmed4872@gmail.com>
2025-12-23 19:40:09 +0530

    React day 1 - initial setup

---
+++ react-day-1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+  <h1>Tanveer Ahmed</h1>
+  <h2>Cohort 2.0 Sheryians Coding School</h2>
+      
+    </>
+  )
+}
+
+export default App
