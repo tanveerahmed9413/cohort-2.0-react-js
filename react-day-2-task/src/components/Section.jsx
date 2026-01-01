f990933a51587076454539faa0ffd08c3c2b58dc
tanveerahmed9413 <tanveerahmed4872@gmail.com>
2026-01-01 22:07:19 +0530

    react-basic-ui project

---
+++ react-day-2-task/src/components/Section.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Center from './center'
+
+const Section = () => {
+  return (
+    <div className='section'>
+      <Center />
+    </div>
+  )
+}
+
+export default Section
